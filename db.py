c0091cc5cf3c32987b128b8aef32f5ac3363ce83
Shafayet Ul Islam <shafayet.islam.61@gmail.com>
2026-03-31 17:47:16 +1030

    setting up postgres in aws rds

--- db.py
+++ db.py
@@ -1,36 +1,41 @@
-import sqlite3
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'expenses.db')
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
+    return psycopg2.connect(DATABASE_URL)
 
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute('''
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS expenses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
             date TEXT NOT NULL,
             merchant TEXT,
             category TEXT,
             total REAL NOT NULL,
             items TEXT,
-            created_at TEXT DEFAULT (datetime('now'))
-            )
-    ''')
+            created_at TIMESTAMP DEFAULT NOW()
+        )
+    """)
 
-    cursor.execute('''
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS budgets (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
             category TEXT UNIQUE NOT NULL,
             monthly_limit REAL NOT NULL
-            )
-    ''')
+        )
+    """)
 
     conn.commit()
+    cursor.close()
     conn.close()
 
 init_db()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from mcp.server.fastmcp import FastMCP
 from tools.extract import extract_receipt
 from tools.expenses import save_expense, get_expenses, get_summary
-from tools.budget import set_budget, get_budget
+from tools.budget import set_budget, check_budget
 
 
 mcp = FastMCP("Trace Agent")
@@ -12,7 +15,9 @@ mcp.tool()(save_expense)
 mcp.tool()(get_expenses)
 mcp.tool()(get_summary)
 mcp.tool()(set_budget)
-mcp.tool()(get_budget)
+mcp.tool()(check_budget)
+
 
 if __name__ == "__main__":
-    mcp.run()
+    import uvicorn
+    uvicorn.run(mcp.streamable_http_app(), host="0.0.0.0", port=8000)
